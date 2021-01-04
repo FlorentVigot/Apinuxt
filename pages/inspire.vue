40d5980a1c8566cecc6923a0c1892e2590fdd5c7
FlorentVigot <vigotflorent@gmail.com>
2021-01-04 21:45:11 +0100

    add logo dnemark liga

--- pages/inspire.vue
+++ pages/inspire.vue
@@ -1,9 +1,13 @@
 <template>
   <v-row>
     <v-col class="text-center">
-      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <img
+        src="../assets/danemarkliga.png"
+        alt="logo-daemark-liga"
+        class="mb-5"
+      />
       <blockquote class="blockquote">
-        &#8220;First, solve the problem. Then, write the code.&#8221;
+        &#8220;il n'y a pas de hasard .&#8221;
         <footer>
           <small>
             <em>&mdash;Flo</em>
