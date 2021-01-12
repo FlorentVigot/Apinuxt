5519185c9890dc9a70672d2ba5437f1543dfcfb4
FlorentVigot <vigotflorent@gmail.com>
2021-01-12 19:02:09 +0100

    scotish add

---
+++ pages/scotish.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img
+        src="../assets/danemarkliga.png"
+        alt="logo-daemark-liga"
+        class="mb-5"
+      />
+      <blockquote class="blockquote">
+        &#8220;voici la liste des match pour aujourd'hui&#8221;
+        <footer>
+          <small>
+            <em>&mdash;Match</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template><script>
+</script>
+<style scoped>
+template {
+  background-color: #00a86b;
+}
+</style>
