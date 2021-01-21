189e9e3259f0eee0689efaeeb0be93b5022b765f
FlorentVigot <vigotflorent@gmail.com>
2021-01-21 18:30:26 +0100

    add btn

--- pages/index.vue
+++ pages/index.vue
@@ -14,12 +14,13 @@
             <h2>Coaching gagnant c'est quoi ?</h2>
             <p>
               Coaching gagnant, c'est un site web, qui regroupe tout les match
-              de Superliga, la ligue danoise ! et la ligue ecossaise !
+              de Superliga, la ligue danoise et la ligue écossaise !
             </p>
           </v-card-text>
           <v-card-actions>
             <v-spacer />
-            <v-btn color="dark" nuxt to="/inspire"> Superliga </v-btn>
+            <v-btn color="dark" nuxt to="/superliga"> Superliga </v-btn>
+            <v-btn color="dark" nuxt to="/scottish"> Ligue écossaise </v-btn>
           </v-card-actions>
         </v-card>
       </v-col>
